2624930099787c4b81726bc9d33370f4f9ae92c4
bgamut <bgamut@gmail.com>
2017-06-27 03:58:35 +0900

    updated responsiveness to bot

--- app.py
+++ app.py
@@ -88,6 +88,7 @@ def webhook():
                             send_message(sender_id, response['msg'])
                         if 'action' in stuff:
                             print ("action!")
+                            re
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
 
