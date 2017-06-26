4af12a1cb387efb1aaeaf125e02e0baabef4ea0f
bgamut <bgamut@gmail.com>
2017-06-27 04:22:21 +0900

    updated responsiveness to bot

--- app.py
+++ app.py
@@ -88,7 +88,8 @@ def webhook():
                             send_message(sender_id, response['msg'])
                         if 'action' in stuff:
                             print ("action!")
-                            if response.get('action')=='start'
+                            if response.get('action'):
+                                print response['action']
                                 start();
             
                 if messaging_event.get("delivery"):  # delivery confirmation
