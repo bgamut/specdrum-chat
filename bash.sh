ea9abfcb69c4f4d955b83c61216c69dc04bf020b
bgamut <bgamut@gmail.com>
2017-06-27 04:07:23 +0900

    updated responsiveness to bot

--- bash.sh
+++ bash.sh
@@ -2,5 +2,6 @@ git pull origin master
 git add .
 git commit -m "updated responsiveness to bot"
 git push -f origin master
+heroku git:remote -a specdrum-chat
 git push heroku specdrum-chat:master
 heroku logs
